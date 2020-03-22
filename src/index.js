76ebf454471613ea80424c7f636d32c60c95ec0f
Valeria Matieva <foystina@gmail.com>
2020-03-22 14:23:50 +0400

    feat: upload task on github

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+var dir = 'LR';
+var res = [];
+if (matrix == [] || matrix === undefined) return [];
+  for (let i = 0; i < matrix.length; i++) {
+    if (dir == 'LR') {
+      for (let j = 0; j < matrix[i].length; j++) {
+       res.push(matrix[i][j]);
+    }
+      dir = 'RL';
+    }
+    else if (dir == 'RL') {
+      matrix[i].reverse();
+      for (let k = 0; k < matrix[i].length; k++) {
+       res.push(matrix[i][k]);
+      }
+    dir = 'LR';
+  }
+  }
+return res;
 }
